0df1418f432d305fdc6440c315eadc9f492068d7
Corey Rigney <coreyrigney@gmail.com>
2024-04-23 10:56:39 -0700

    Lighting work, starting level design work

---
+++ Scenes/torch.gd
@@ -0,0 +1,26 @@
+@tool
+extends Node2D	
+
+var rangeZMin: int
+var rangeZMax: int
+var layerMin: int
+var layerMax: int
+
+var shadowEnabled: bool
+var shadowColor: Color
+
+var lightTexture: Texture2D
+var lightOffset: Vector2
+var lightTexScale: float
+
+var lightColor: Color
+var lightEnergy: float 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
